7b57e1aa646abc8a97d6119cb24aef30f8faee7c
suman <sumanbhamu@rocketmail.com>
2017-01-07 05:05:03 -0800

    Update UserService.js

--- colabfrontend/WebContent/c_user/UserService.js
+++ colabfrontend/WebContent/c_user/UserService.js
@@ -1,24 +1,60 @@
 'user strict'; /* strictly follows the case i.e--case sensitive */
 
-app.factory('UserService', [
+app.service('UserService', [
 		'$http',
 		'$q',
 		'$rootScope',
 		function($http, $q, $rootScope) {
-			console.log("User SERvices....//")
-			var BASE_URL = 'http://localhost:8088/cobackend/'
+			console.log("User SERvices.... in user service//")
+			var BASE_URL = 'http://localhost:8088/cobackend'
 			return {
-				getAllUsers : function() {
-					console.log(" calling fetch all Users..//")
-					return $http.get(BASE_URL + '/usreL').then(
+				/*getAllUsers : function() {
+					console.log(" calling fetch all Users.. in user service.js//")
+					return $http.get(BASE_URL + '/usreL')
+					.then(
 							function(response) {
 								return response.data;
 							}, null
+							
+							function(response){
+								console.log('Calling all the users in user service.js ')
+								return response.data;
+							},
+							function(errResponse){
+								console.error('Error while fetching Users....userservice.js');
+								return $q.reject(errResponse);
+												} 
 
 					);
-				},
-
-				myProfile : function() {
+				},*/
+				
+				fetchAllUsers:function(){
+					return $http.get(BASE_URL+'/users')
+					.then(
+							function(response){
+								console.log('Calling user in user service ')
+								return response.data;
+							},
+							function(errResponse){
+								console.error('Error while fetching UserDetails');
+								return $q.reject(errResponse);
+							}				
+			);
+		},
+		createUser:function(user){
+			return $http.post(BASE_URL+'/user/',user)
+			.then(
+					function(response){
+						console.log('calling createUser in UserService');
+						return response.data;
+					},
+					function(errResponse){
+						console.error('Error while creating User');
+						return $q.reject(errResponse);
+					}				
+		);
+		},
+				/*myProfile : function() {
 					console.log("  Users.. profile..in user service//")
 					return $http.get(BASE_URL + '/myProfile').then(
 							function(response) {
@@ -26,7 +62,7 @@ app.factory('UserService', [
 							}, null
 
 					);
-				},
+				},*/
 
 				accept : function(id) {
 					console.log("   calling approved.... in user service//")
@@ -49,19 +85,20 @@ app.factory('UserService', [
 							);
 				},
 
-				register : function(user) {
+				/*register : function(user) {
 					console.log("   calling register....in user servive//")
 					return $http.post(BASE_URL + '/register/', user).then(
 							function(response) {
+								console.log('calling register User in UserService');
 								return response.data;
-							}, /*null*/
+							}, null
 							function(errResponse){
 								console.error('Error while creating User');
 								return $q.reject(errResponse);
 							}	
 
-					);
-				},
+					);*/
+				/*},*/
 
 				updateUser : function(user) {
 					console.log("   updating userr....in user service//")
