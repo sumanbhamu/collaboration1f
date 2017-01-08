f9926455aef4c88f263f33cf7b510d960c2cbec9
suman <sumanbhamu@rocketmail.com>
2017-01-08 06:45:52 -0800

    Update UserController.js

--- colabfrontend/WebContent/c_user/UserController.js
+++ colabfrontend/WebContent/c_user/UserController.js
@@ -1,69 +1,78 @@
+	
 'use strict';
 
-app.controller(	'UserController', [	'$scope', 'UserService', '$location','$rootScope',/*'$cookieStore',*/
+app
+		.controller(
+				'UserController',
+				[
+						'$scope',
+						'UserService',
+						'$location',
+						'$rootScope',/* '$cookieStore', */
 						'$http',
 						function($scope, UserService, $location, $rootScope,
-								/*$cookieStore,*/$http) {
+						/* $cookieStore, */$http) {
+							
+
+							console.log('In LoginController.....usercontroler.js....... ')
+									//$scope.message="You successfully logged In"
+										
+									$rootScope.isLoggedIn="true"
+							
+							
 							console.log("UserController.....js")
 							var self = this;
-							self.user = {userid : '',username : '', password : '', cpassword : '',	phno : '',status:'',
-								 emailid : '',isOnline : '',	role : '',
-								errorCode : '',	errorMessage : '',/* file : ''*/
+							self.user = {
+								userid : '',
+								username : '',
+								password : '',
+								cpassword : '',
+								phno : '',
+								status : '',
+								emailid : '',
+								isOnline : '',
+								role : '',
+								errorCode : '',
+								errorMessage : '',/* file : '' */
 							};
 							self.users = [];
-							
-							 $scope.orderByMe = function(x) {
-							        $scope.myOrderBy = x;
-							    }
-					
-							/*self.getAllUsers = function() {
-								console.log("fetchAllUsers..in controlller.js.")
+
+							$scope.orderByMe = function(x) {
+								$scope.myOrderBy = x;
+							}
+
+
+							/* FETCH ALL USERS LIST........ .................................*/
+							self.fetchAllUsers = function() {
 								UserService
-										.getAllUsers()
+										.fetchAllUsers()
 										.then(
 												function(d) {
-													self.userL = d;
+													self.users = d;
+													console.log("get all the users in usercontroller.js....")
 												},
 												function(errResponse) {
 													console
-															.error('Error while fetching Users ,,,,controller.js');
+															.error('Error while fetching Users');
 												});
 							};
-							
-							//self.getAllUsers();
+							self.fetchAllUsers();
 
-							self.register = function(user) {
-								console.log("createUser... in user controller")
+							/* CREATE USER ....... ................................................*/
+							self.createUser = function(user) {
+
 								UserService
-										.register(user)
+										.createUser(user)
 										.then(
-												self.getAllUsers,
+												
+												self.fetchAllUsers,
+												
 												function(errResponse) {
 													console
-															.error('Error while creating User.');
+															.error('Error while creating User...');
 												});
+								
 							};
-*/			
-							 
-							 /* FETCH ALL USERS LIST........ */
-								self.fetchAllUsers = function() {
-									UserService.fetchAllUsers().then(function(d) {
-										self.users = d;
-									}, function(errResponse) {
-										console.error('Error while fetching Users');
-									});
-								};
-								self.fetchAllUsers();
-
-					/* CREATE USER .......*/
-								self.createUser = function(user) {
-									
-									UserService.createUser(user)
-									.then(self.fetchAllUsers,
-											function(errResponse) {
-												console.error('Error while creating User...');
-											});
-								};
 
 							self.myProfile = function() {
 								console.log("myProfile...")
@@ -72,14 +81,15 @@ app.controller(	'UserController', [	'$scope', 'UserService', '$location','$rootS
 										.then(
 												function(d) {
 													self.user = d;
-													$location.path("/myProfile")
+													$location
+															.path("/myProfile")
 												},
 												function(errResponse) {
 													console
 															.error('Error while fetch profile.');
 												});
 							};
-							
+
 							self.accept = function(id) {
 								console.log("accept...")
 								UserService
@@ -88,105 +98,90 @@ app.controller(	'UserController', [	'$scope', 'UserService', '$location','$rootS
 												function(d) {
 													self.user = d;
 													self.fetchAllUsers
-													$location.path("/manage_users")
+													$location
+															.path("/manage_users")
 													alert(self.user.errorMessage)
-													
+
 												},
-												
+
 												function(errResponse) {
 													console
 															.error('Error while updating User.');
 												});
 							};
-							
-							self.reject = function( id) {
+
+							self.reject = function(id) {
 								console.log("reject...")
 								var reason = prompt("Please enter the reason");
-								UserService
-										.reject(id,reason)
-										.then(
-												function(d) {
-													self.user = d;
-													self.fetchAllUsers
-													$location.path("/manage_users")
-													alert(self.user.errorMessage)
-													
-												},
-												null );
+								UserService.reject(id, reason).then(
+										function(d) {
+											self.user = d;
+											self.fetchAllUsers
+											$location.path("/manage_users")
+											alert(self.user.errorMessage)
+
+										}, null);
 							};
 
 							self.updateUser = function(user, id) {
 								console.log("updateUser...")
-								UserService
-										.updateUser(user, id)
-										.then(
-												self.fetchAllUsers,
-												null);
+								UserService.updateUser(user, id).then(
+										self.fetchAllUsers, null);
 							};
 
-/*							self.authenticate = function(user) {
-								console.log("authenticate...")
-								UserService
-										.authenticate(user)
-										.then(
+							//self.fetchAllUsers();
 
-												function(d) {
+							self.login = function() {
+								{
+									console.log('login validation????????',self.user);
+									self.authenticate(self.user);
+								}
 
-													self.user = d;
-													console
-															.log("user.errorCode: "
-																	+ self.user.errorCode)
-													if (self.user.errorCode == "404")
+							};
+							
+							
+							self.authenticate = function(user) {
+								UserService.authenticate(user)
+								.then(function(d) {
+									self.user = d;
+								
+									if (self.user.errorCode == "404")
 
-													{
-														alert(self.user.errorMessage)
+									{
+										alert(self.user.errorMessage)
+										console.log("error in loggin in user controller.js..")
 
-														self.user.userid = "";
-														self.user.password = "";
+										self.user.userid = "";
+										self.user.password = "";
 
-													} else { //valid credentials
-														console
-																.log("Valid credentials. Navigating to home page")
-														$rootScope.currentUser = self.user
-														$http.defaults.headers.common['Authorization'] = 'Basic '
-																+ $rootScope.currentUser;
-														$cookieStore
-																.put(
-																		'currentUser',
-																		$rootScope.currentUser);
-														$location.path('/');
-
-													}
-
-												},
-												function(errResponse) {
-
-													console
-															.error('Error while authenticate Users');
-												});
-							};*/
-
+									} else { // valid
+										// credentials
+										
+											console.log("user login in user controller.js.....")
+											$rootScope.isLoggedIn="true"
+											$location.path('/');
+									    	console.log("Valid credentials. Navigating to home page")
+									
+									}
+								},
+									function(errResponse) {
+									console.error('Error while authenticating User...');
+								});
+							};
+							
+							
+							
+							
 							self.logout = function() {
 								console.log("logout")
 								$rootScope.currentUser = {};
 								$cookieStore.remove('currentUser');
 								UserService.logout()
+										
 								$location.path('/');
 
 							}
 
-						
-
-							self.fetchAllUsers();
-
-							self.login = function() {
-								{
-									console.log('login validation????????',
-											self.user);
-									self.authenticate(self.user);
-								}
-
-							};
 
 							self.submit = function() {
 								{
@@ -197,149 +192,70 @@ app.controller(	'UserController', [	'$scope', 'UserService', '$location','$rootS
 							};
 
 							self.reset = function() {
+								console.log('reset user....',self.user);
 								self.user = {
-										userid : '',username : '', password : '', cpassword : '',	phno : '',status:'',
-										 emailid : '',isOnline : '',	role : '',
-										errorCode : '',	errorMessage : '',/* file : ''*/
+									userid : '',
+									username : '',
+									password : '',
+									cpassword : '',
+									phno : '',
+									status : '',
+									emailid : '',
+									isOnline : '',
+									role : '',
+									errorCode : '',
+									errorMessage : '',/* file : '' */
 								};
-								$scope.myForm.$setPristine(); // reset Form
+								//$scope.myForm.$setPristine(); // reset Form
 							};
 
 						} ]);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*'use strict';
-
-app.controller('UserController',   ['$scope','UserService','$location','$rootScope','$cookieStore','$http',
-                                 function($scope,UserService,$location,$rootScope,$cookieStore,$http){
-	
-										console.log("User controller.....")
-										
-										// self--refers to UserController
-										var self=this;
-										self.user={
-												userid:'',
-												username:'',
-												password:'',
-												cpassword:'',
-												emailid:'',
-												phno:'',
-												role:'',
-												status:'',
-												reason:'',
-												isOnline:'',
-												errorCode:'',
-												errorMessage:'',
-												file:''
-													
-										};
-										self.users=[];
-										
-										for sorting
-										$scope.orderByMe=function(x){
-											$scope.myOrderBy=x;
-										}
-										
-										
- to fetch all the users										
-										self.getAllUsers=function(){
-											console.log("fetching all user......")
-											UserService
-													.getAllUsers()
-													.then(
-															function(d)
-						                    				{
-																self.users=d;
-															},
-															function(errResponse)
-															{
-																console.error('Error while fetching Users..');
-															}
-														);
-													
-										};
-						                
-										
-										
-										
- to create an user---register											
-										self.register = function(user) {
-											console.log("createUser...")
-											UserService
-													.register(user)
-													.then(
-															self.getAllUsers,
-															function(errResponse) {
-																console
-																		.error('Error while creating User....**');
-															});
-										};
-									
-										
-										
-										
-
-										self.myProfile = function() {
-											console.log("myProfile...")
-											UserService
-													.myProfile()
-													.then(
-															function(d) {
-																self.user = d;
-																$location.path("/myProfile")
-															},
-															function(errResponse) {
-																console
-																		.error('Error while fetch profile.*****');
-															});
-										};
-										
-
-										
-										
-										
-										
-										
-										
-}
-
-]
-
-);*/
+/*
+ * 'use strict';
+ * 
+ * app.controller('UserController',
+ * ['$scope','UserService','$location','$rootScope','$cookieStore','$http',
+ * function($scope,UserService,$location,$rootScope,$cookieStore,$http){
+ * 
+ * console.log("User controller.....")
+ *  // self--refers to UserController var self=this; self.user={ userid:'',
+ * username:'', password:'', cpassword:'', emailid:'', phno:'', role:'',
+ * status:'', reason:'', isOnline:'', errorCode:'', errorMessage:'', file:''
+ *  }; self.users=[];
+ * 
+ * for sorting $scope.orderByMe=function(x){ $scope.myOrderBy=x; }
+ * 
+ * 
+ * to fetch all the users self.getAllUsers=function(){ console.log("fetching all
+ * user......") UserService .getAllUsers() .then( function(d) { self.users=d; },
+ * function(errResponse) { console.error('Error while fetching Users..'); } );
+ *  };
+ * 
+ * 
+ * 
+ * 
+ * to create an user---register self.register = function(user) {
+ * console.log("createUser...") UserService .register(user) .then(
+ * self.getAllUsers, function(errResponse) { console .error('Error while
+ * creating User....**'); }); };
+ * 
+ * 
+ * 
+ * 
+ * 
+ * self.myProfile = function() { console.log("myProfile...") UserService
+ * .myProfile() .then( function(d) { self.user = d; $location.path("/myProfile") },
+ * function(errResponse) { console .error('Error while fetch profile.*****');
+ * }); };
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ *  }
+ *  ]
+ *  );
+ */
