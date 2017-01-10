6327ac8c9d4b3b2fa45c77cfd8d4c3ed0d2ce8a5
suman <sumanbhamu@rocketmail.com>
2017-01-10 09:41:37 -0800

    Update JobDAO.java

--- cobackend/src/main/java/com/suman/dao/JobDAO.java
+++ cobackend/src/main/java/com/suman/dao/JobDAO.java
@@ -3,17 +3,27 @@ package com.suman.dao;
 import java.util.List;
 
 import com.suman.model.Job;
+import com.suman.model.JobApplied;
 
 public interface JobDAO {
 
 	
-	public boolean save(Job job); //posting/adding a job
+	public boolean save(Job job); //posting/adding a job	
+	public boolean save(JobApplied jobApplied); //applying for job
 
-	public boolean update(Job job); //updating job
+	public boolean update(Job job); //updating job	
+	public boolean update(JobApplied jobApplied); //updating job application
 	
-	public Job get(String jobID);
+	public List<Job> getAllAvailableJobs(); //list opened jobs
+		
+	public Job getJobDetails(String id);  // get all the job details
 	
-	public List<Job> list();
+	public List<Job> getMyAppliedJobs(String userId); //listing jobs of a particular user
+	
+	
+	public JobApplied getJobApplied(String userId,String jobId); //get particular user's applied 
+	
+	public JobApplied getJobApplied(String jobId); //get all the job applications
 	
 	public boolean delete(Job job);
 
