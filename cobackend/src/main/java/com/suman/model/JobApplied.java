2c2efd1082c463a3c6f02521f35f1dc3c47e1e1d
suman <sumanbhamu@rocketmail.com>
2017-01-13 04:08:35 -0800

    Update JobApplied.java

--- cobackend/src/main/java/com/suman/model/JobApplied.java
+++ cobackend/src/main/java/com/suman/model/JobApplied.java
@@ -3,10 +3,12 @@ package com.suman.model;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-
+import org.hibernate.annotations.ValueGenerationType;
 import org.springframework.stereotype.Component;
 
 @Entity
@@ -15,32 +17,36 @@ import org.springframework.stereotype.Component;
 
 public class JobApplied extends BaseDomain{
 
-    @Id
-	private String id;
-	private String user_id;
-	private String job_id;
+    public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public String getJobid() {
+		return jobid;
+	}
+	public void setJobid(String jobid) {
+		this.jobid = jobid;
+	}
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+
+	private int id;
+    
+	private String userid;
+	private String jobid;
 
 	private Date date_applied;
 	private String emailid;
 	private String remark;  /*role-student,alumni,admin,employee*/
 	private char status;   /* status-new,accepted,rejected  */
-	public String getId() {
+	
+	public int getId() {
 		return id;
 	}
-	public void setId(String id) {
+	public void setId(int id) {
 		this.id = id;
-	}
-	public String getUser_id() {
-		return user_id;
-	}
-	public void setUser_id(String user_id) {
-		this.user_id = user_id;
-	}
-	public String getJob_id() {
-		return job_id;
-	}
-	public void setJob_id(String job_id) {
-		this.job_id = job_id;
 	}
 	public Date getDate_applied() {
 		return date_applied;
